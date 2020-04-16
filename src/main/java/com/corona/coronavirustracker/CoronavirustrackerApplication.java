e3d8475f15ce6847c31ba0264c0c29060d8a1ce8
mathurvarun_1984 <varun.m@trezi.com>
2020-04-16 17:05:56 +0530

    Added coronavirustracker

---
+++ src/main/java/com/corona/coronavirustracker/CoronavirustrackerApplication.java
@@ -0,0 +1,13 @@
+package com.corona.coronavirustracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronavirustrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronavirustrackerApplication.class, args);
+	}
+
+}
